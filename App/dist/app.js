7907e3dc3dc40f7a67f3a5a47ea8cd2f5b496d3c
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2025-08-20 01:39:26 -0300

    first commit

---
+++ App/dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nome;
+let idade;
+nome = 'Rodrigo';
+idade = 36;
+console.log(`nome: ${nome}, idade: ${idade}`);
+//# sourceMappingURL=app.js.map
